0d5377d3d3710373a7be18aba80b19d3484a50ef
rk0576 <70932901+rk0576@users.noreply.github.com>
2020-12-02 12:10:37 +0300

    Add files via upload

---
+++ pr.2.1.py
@@ -0,0 +1,7 @@
+import math
+n=int(input("introduceti numarul"))
+if n<((10*70)-1):
+    r=math.sqrt(n)
+    p=int(r)
+print(n)
+print(n-(p**2))
---
+++ pr1.1.py
@@ -0,0 +1,8 @@
+s=str(input("introduceti numele si prenumele="))
+a,b=s.split()
+print(a)
+print(b)
+if (a.title==a)and (b.title==b):
+    print("numele este corect")
+else:
+    print("numele nu este corect")
